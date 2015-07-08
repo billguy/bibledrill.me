f541cf46c6a4d456ab4214870480a73c3395f9a2
David John <dj@dbjohn.com>
2015-07-08 17:32:13 -0500

    Refactor deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -13,7 +13,7 @@ set :tmp_dir, "#{APP_CONFIG['deploy_to']}/tmp"
 set :whenever_command,      ->{ [:bundle, :exec, :whenever] }
 set :whenever_identifier, ->{ "#{fetch(:application)}_#{fetch(:stage)}" }
 
-set :linked_files, fetch(:linked_files, []).push('config/database.yml')
+set :linked_files, fetch(:linked_files, []).push('config/database.yml', 'config/config.yml')
 
 # Default value for linked_dirs is []
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'private', 'public/ckeditor_assets')
