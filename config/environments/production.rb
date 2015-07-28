30307727e0e160799403fa63fdda168b71496f43
David John <djohn@arch-no.org>
2015-07-28 10:59:08 -0500

    Fixes memcache

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -56,7 +56,7 @@ Rails.application.configure do
 
   # Use a different cache store in production.
   # config.cache_store = :mem_cache_store
-  config.cache_store = :dalli_store
+  config.cache_store = Dalli::Client.new(Dir.home + "/memcached.sock")
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
   # config.action_controller.asset_host = 'http://assets.example.com'
