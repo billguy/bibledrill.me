af652deb7d6b4ed18414986cbf387651fe270cb4
David John <djohn@arch-no.org>
2015-07-28 14:55:36 -0500

    Added display multiple verses

--- app/controllers/verses_controller.rb
+++ app/controllers/verses_controller.rb
@@ -8,12 +8,27 @@ class VersesController < ChaptersController
   end
 
   def show
-    @verse = Rails.cache.fetch("book/#{@book.name}/chapter/#{@chapter.number}/verses/#{params[:id]}"){ @chapter.verse(params[:id]) }
-    @page_title = @verse.title
+    @verses = Rails.cache.fetch("book/#{@book.name}/chapter/#{@chapter.number}/verses/#{parsed_verses.join}"){ @chapter.verses(parsed_verses) }
     add_breadcrumb "Chapter #{@chapter.number}", book_chapters_path(book_id: @book.permalink, chapter_id: @chapter.number), title: "Chapter #{@chapter.number} verses", remote: true
-    add_breadcrumb "Verse #{@verse.number}", book_chapter_verses_path(book_id: @book.permalink, chapter_id: @chapter.number), title: "Verse #{@verse.number}", remote: true
+    if @verses.length > 1
+        @page_title = @chapter.title
+    else
+        @page_title = @verses.first.title
+        add_breadcrumb "Verse #{@verses.first.number}", book_chapter_verses_path(book_id: @book.permalink, chapter_id: @chapter.number), title: "Verse #{@verses.first.number}", remote: true
+    end
     rescue Kj::Iniquity
       redirect_to root_path, flash: { error: "No such verses(s) '#{params[:id]}' in '#{params[:book_id]}:#{params[:chapter_id]}'" }
   end
+
+  private
+
+    def parsed_verses
+      @parsed_verses ||= begin
+        verses = params[:id].gsub(/\s+/, "")
+        single_verses = verses.split(',').reject{|verse| verse =~ /\d{1,}\-\d{1,}/ }.map(&:to_i)
+        ranges = verses.scan(/\d{1,}\-\d{1,}/).map{|range| Range.new(*range.split("-").map(&:to_i)).to_a }
+        [single_verses, ranges].flatten.compact
+      end
+    end
 
 end
--- app/views/verses/show.json.jbuilder
+++ app/views/verses/show.json.jbuilder
@@ -1,3 +1,11 @@
-json.number @verse.number
-json.text @verse.text
-json.chapter_url book_chapter_path(book_id: @book.permalink, chapter_id: @verse.chapter_id, format: :json)
+if @verses.length == 1
+  json.number @verses.number
+  json.text @verses.text
+  json.chapter_url book_chapter_path(book_id: @book.permalink, chapter_id: @verses.chapter_id, format: :json)
+else
+  json.array! @verses do |verse|
+    json.number verse.number
+    json.text verse.text
+    json.chapter_url book_chapter_path(book_id: @book.permalink, chapter_id: verse.chapter_id, format: :json)
+  end
+end
