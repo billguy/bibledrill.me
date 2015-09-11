696b619ebca047e99d432f35c189b3875cfb9935
David John <djohn@arch-no.org>
2015-09-11 07:48:16 -0500

    Updated delivery_method

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -94,6 +94,7 @@ Rails.application.configure do
 
   config.action_mailer.default_url_options = { protocol: 'https', host: 'bibledrill.me' }
 
+  config.action_mailer.delivery_method = :postmark
   config.action_mailer.postmark_settings = { api_token: ENV['postmark_token'] }
 
 end
