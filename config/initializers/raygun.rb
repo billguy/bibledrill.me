a7b3df0c606e19e946492423cce00be8d9a8308f
David John <dj@dbjohn.com>
2015-11-11 09:26:12 -0600

    Updated raygun ignore

--- config/initializers/raygun.rb
+++ config/initializers/raygun.rb
@@ -2,6 +2,8 @@ Raygun.setup do |config|
   config.api_key = ENV['RAYGUN_APIKEY']
   config.filter_parameters = Rails.application.config.filter_parameters
 
+  config.ignore  << ['ActionController::BadRequest', 'ActionView::MissingTemplate']
+
   # The default is Rails.env.production?
   # config.enable_reporting = !Rails.env.development? && !Rails.env.test?
 end
