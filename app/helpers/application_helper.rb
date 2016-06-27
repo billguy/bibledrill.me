64169f31773f6dfc87bfc7fcf4d934e924b3c79c
David John <djohn@arch-no.org>
2016-06-27 10:26:15 -0500

    find_asset fix

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,13 @@
 module ApplicationHelper
 
+  def asset_exists?(asset_name)
+    if Rails.application.assets
+      Rails.application.assets.find_asset(asset_name)
+    else
+      Rails.application.assets_manifest.assets[asset_name]
+    end
+  end
+
   def default_meta_tags
     {
       site: 'BibleDrill.me',
