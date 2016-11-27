b91ef1475bc698d06616c601ced70c5c12b1026d
David John <dj@dbjohn.com>
2016-11-27 10:29:48 -0600

    Added old and new search params

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -15,10 +15,13 @@ class BooksController < KjController
 
   def search
     if Rails.env.production?
-      @verses = Verse.includes(chapter: :book).where('text ~* ?', '\y%s\y' % params[:q]).order(id: :asc).page params[:page]
+      @verses = Verse.includes(chapter: :book).where('text ~* ?', '\y%s\y' % params[:q])
     else
-      @verses = Verse.includes(chapter: :book).where('text like ?', "%#{params[:q]}%").order(id: :asc).page params[:page]
+      @verses = Verse.includes(chapter: :book).where('text like ?', "%#{params[:q]}%")
     end
+    @verses = @verses.old_testament if params[:old_testament].present? && !params[:new_testament]
+    @verses = @verses.new_testament if params[:new_testament].present? && !params[:old_testament]
+    @verses = @verses.order(id: :asc).page params[:page]
   end
 
 end
--- app/models/verse.rb
+++ app/models/verse.rb
@@ -12,8 +12,12 @@ class Verse < ActiveRecord::Base
 
   delegate :book_permalink, to: :chapter
   delegate :book_name, to: :chapter
+  delegate :book_id, to: :chapter
   delegate :chapter_id, to: :chapter
   delegate :chapter_number, to: :chapter
+
+  scope :old_testament, ->{ includes(:chapter).where(chapters: { book_id: 1..39 } ) }
+  scope :new_testament, ->{ includes(:chapter).where(chapters: { book_id: 40..66 } ) }
 
   def title
     "#{book_name} #{chapter_number}:#{number}"
@@ -27,20 +31,20 @@ class Verse < ActiveRecord::Base
     31102
   end
 
-  def self.pages
-    last.page
-  end
-
-  def self.random
-    find(rand(1...count))
-  end
-
   def prev
     id == 1 ? Verse.includes(chapter: :book).last : Verse.includes(chapter: :book).find_by_id(id-1)
   end
 
   def next
     id == self.class.count ? Verse.includes(chapter: :book).first : Verse.includes(chapter: :book).find_by_id(id+1)
+  end
+
+  def self.pages
+    last.page
+  end
+
+  def self.random
+    find(rand(1...count))
   end
 
 end
